54445bc24d6c6be38515893cc0a8c7661e6f6cfe
Viktoriia Pavlushka <146458258+ViktoriiaPavlushka@users.noreply.github.com>
2024-05-17 14:24:22 +0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,1 +1,8 @@
-public class Node {    int value;    Node next;    public Node(int value) {        this.value = value;        this.next = null;    }}
+public class Node {
+    int value;
+    Node next;
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
