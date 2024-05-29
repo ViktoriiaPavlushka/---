8d41e72d06c5cfc999cb11441898417c68405a86
Viktoriia Pavlushka <146458258+ViktoriiaPavlushka@users.noreply.github.com>
2024-05-29 12:01:43 +0300

    Update AccountManager.java

--- src/main/java/bank2/bank_ioc/service/AccountManager.java
+++ src/main/java/bank2/bank_ioc/service/AccountManager.java
@@ -1,1 +1,37 @@
-package bank2.bank_ioc.service;import bank2.bank_ioc.models.Account;import bank2.bank_ioc.models.Currency;import bank2.bank_ioc.repository.AccountRepository;import org.springframework.stereotype.Component;import java.util.List;@Componentpublic class AccountManager {    private final AccountRepository repository;    public AccountManager(AccountRepository repository){        this.repository = repository;    }    public Account OpenAccount(String Name, String Surname, double Balance, Currency currency){        Account user = new Account(Name, Surname, Balance, currency);        repository.Add(user);        return user;    }    public Account GetAccount(String ID){        return repository.Get(ID);    }    public void RemoveAccount(String ID){        repository.Remove(ID);    }    public void AddAccount(Account user){        repository.Add(user);    }    public List<Account> GetAllAccounts() {        return repository.getAll();    }}
+package bank2.bank_ioc.service;
+
+import bank2.bank_ioc.models.Account;
+import bank2.bank_ioc.models.Currency;
+import bank2.bank_ioc.repository.AccountRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AccountManager {
+    private final AccountRepository repository;
+
+    public AccountManager(AccountRepository repository){
+        this.repository = repository;
+    }
+
+    public Account OpenAccount(String Name, String Surname, double Balance, Currency currency){
+        Account user = new Account(Name, Surname, Balance, currency);
+        repository.Add(user);
+        return user;
+    }
+
+    public Account GetAccount(String ID){
+        return repository.Get(ID);
+    }
+    public void RemoveAccount(String ID){
+        repository.Remove(ID);
+    }
+    public void AddAccount(Account user){
+        repository.Add(user);
+    }
+    public List<Account> GetAllAccounts() {
+        return repository.getAll();
+    }
+
+}
